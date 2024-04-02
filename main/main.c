fa9988c31c58eecad4d6e80f379af046f842a3b4
Renato Freitas <freitas-renato@outlook.com>
2024-04-01 22:51:17 -0400

    Update project to esp-idf v5.2.1

--- main/main.c
+++ main/main.c
@@ -6,21 +6,23 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include <stdio.h>
 #include "sdkconfig.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <esp_log.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_freertos_hooks.h"
 #include "freertos/semphr.h"
 #include "esp_system.h"
-#include "esp_spi_flash.h"
-
-#include "esp_log.h"
-
-#include "esp_freertos_hooks.h"
 #include "driver/gpio.h"
+#include "esp_timer.h"
 
 #include "lvgl.h"
-// #include "lvgl/lvgl.h"
+
 #include "lvgl_helpers.h"
 
 #include "homescreen.h"
@@ -39,21 +41,21 @@ void app_main(void) {
     ESP_LOGI(TAG, "Hello world!");
     // printf("Hello world!\n");
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    ESP_LOGI(TAG, "This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+    // /* Print chip information */
+    // esp_chip_info_t chip_info;
+    // esp_chip_info(&chip_info);
+    // ESP_LOGI(TAG, "This is %s chip with %d CPU core(s), WiFi%s%s, ",
+    //         CONFIG_IDF_TARGET,
+    //         chip_info.cores,
+    //         (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+    //         (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
 
-    ESP_LOGI(TAG, "silicon revision %d, ", chip_info.revision);
+    // ESP_LOGI(TAG, "silicon revision %d, ", chip_info.revision);
 
-    ESP_LOGI(TAG, "%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    // ESP_LOGI(TAG, "%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+    //         (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    ESP_LOGI(TAG, "Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+    // ESP_LOGI(TAG, "Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
 
     // Create LVGL task
     xTaskCreatePinnedToCore(gui_task, "GUI", 4096*2, NULL, 0, NULL, 1);
@@ -81,17 +83,17 @@ static void gui_task(void* arg) {
     lv_color_t* buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
     assert(buf2 != NULL);
 
-    static lv_disp_buf_t disp_buf;
+    static lv_disp_draw_buf_t disp_buf;
     uint32_t size_in_px = DISP_BUF_SIZE;
 
     // Initialize framebuffer using double buffering method
-    lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+    lv_disp_draw_buf_init(&disp_buf, buf1, buf2, size_in_px);
 
     // Initialize the display
     lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
     disp_drv.flush_cb = disp_driver_flush;  // using lvgl esp32 display driver preset for ILI9341
-    disp_drv.buffer = &disp_buf;
+    disp_drv.draw_buf = &disp_buf;
 
     lv_disp_drv_register(&disp_drv);
 
--- main/views/src/homescreen.c
+++ main/views/src/homescreen.c
@@ -25,61 +25,62 @@ static lv_obj_t* screen = NULL;
 
 
 /**** Private screen functions ****/
-static void move_logo_animation(uint32_t duration);
+// static void move_logo_animation(uint32_t duration);
 
 
 void homescreen_init(void) {
     // Init screen style
     lv_style_init(&_screen_style);
-    lv_style_set_bg_color(&_screen_style, LV_STATE_DEFAULT, lv_color_hex(0x000000));
-    lv_style_set_border_width(&_screen_style, LV_STATE_DEFAULT, 0);    
-    lv_style_set_border_color(&_screen_style, LV_STATE_DEFAULT, lv_color_hex(0x000000));
-    lv_style_set_radius(&_screen_style, LV_STATE_DEFAULT, 0);
+    // lv_style_set_bg_color(&_screen_style, LV_STATE_DEFAULT, lv_color_hex(0x000000));
+    lv_style_set_bg_color(&_screen_style, lv_color_hex(0x000000));
+    lv_style_set_border_width(&_screen_style, 0);    
+    lv_style_set_border_color(&_screen_style, lv_color_hex(0x000000));
+    lv_style_set_radius(&_screen_style, 0);
 
 
     // Create blank screen with black background style
-    screen = lv_obj_create(lv_scr_act(), NULL);
+    screen = lv_obj_create(lv_scr_act());
     lv_obj_set_size(screen, 240, 320);
-    lv_obj_add_style(screen, LV_OBJ_PART_MAIN, &_screen_style);
+    lv_obj_add_style(screen, &_screen_style, 0);
     lv_scr_load(screen);
 
     // Place Instrutherm logo
-    lv_obj_t* img = lv_img_create(screen, NULL);
+    lv_obj_t* img = lv_img_create(screen);
     lv_img_set_src(img, &logo_dither);
-    lv_obj_align(img, NULL, LV_ALIGN_IN_TOP_MID, 0, 40);
+    lv_obj_align(img, LV_ALIGN_TOP_MID, 0, 40);
 
 
-    move_logo_animation(1000);
+    // move_logo_animation(1000);
 }
 
 
 
-static void move_logo_animation(uint32_t duration) {
-    // Get image from current screen 
-    lv_obj_t* img = lv_obj_get_child(screen, NULL);
+// static void move_logo_animation(uint32_t duration) {
+//     // Get image from current screen 
+//     lv_obj_t* img = lv_obj_get_child(screen, NULL);
 
-    // Create animation
-    lv_anim_t a;
-    lv_anim_init(&a);
+//     // Create animation
+//     lv_anim_t a;
+//     lv_anim_init(&a);
 
-    lv_anim_set_exec_cb(&a, (lv_anim_exec_xcb_t)lv_obj_set_y);
-    lv_anim_set_var(&a, img);
-    lv_anim_set_time(&a, duration);
+//     lv_anim_set_exec_cb(&a, (lv_anim_exec_xcb_t)lv_obj_set_y);
+//     lv_anim_set_var(&a, img);
+//     lv_anim_set_time(&a, duration);
 
-    // Set animation play back to get the logo back to the top of the screen
-    lv_anim_set_playback_delay(&a, 100);
-    lv_anim_set_playback_time(&a, duration);
+//     // Set animation play back to get the logo back to the top of the screen
+//     lv_anim_set_playback_delay(&a, 100);
+//     lv_anim_set_playback_time(&a, duration);
 
-    // Get screen height
-    lv_coord_t screen_height = lv_obj_get_height(screen);
+//     // Get screen height
+//     lv_coord_t screen_height = lv_obj_get_height(screen);
 
-    // Go to the bottom and come back up with 40px padding
-    lv_anim_set_values(&a, 40, screen_height - 40 - lv_obj_get_height(img));
+//     // Go to the bottom and come back up with 40px padding
+//     lv_anim_set_values(&a, 40, screen_height - 40 - lv_obj_get_height(img));
 
-    // Repeat forever
-    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+//     // Repeat forever
+//     lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
 
-    // Start animation after 2s
-    lv_anim_set_delay(&a, 2000);
-    lv_anim_start(&a);
-}
+//     // Start animation after 2s
+//     lv_anim_set_delay(&a, 2000);
+//     lv_anim_start(&a);
+// }
